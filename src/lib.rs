d989c9841cbb3336afd7a2a823eb6e61af711164
huarkiou <huarkiou@outlook.com>
2025-06-28 11:29:27 +0800

    修改公开接口

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use crate::value::Value;
-
 mod context;
 mod dict;
 mod error;
@@ -7,20 +5,4 @@ mod number;
 mod stack;
 mod value;
 
-pub struct Json {
-    value: Value,
-}
-
-impl Json {
-    pub fn parse(json: &str) -> Self {
-        Self {
-            value: Value::parse(json).unwrap(),
-        }
-    }
-}
-
-impl std::fmt::Display for Json {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.value)
-    }
-}
+pub use value::Value;
