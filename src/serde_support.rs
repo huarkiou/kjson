9cfb30b3f3c2856631babbade018716826042ab1
huarkiou <huarkiou@outlook.com>
2025-07-01 15:55:48 +0800

    序列化Value

--- src/serde_support.rs
+++ src/serde_support.rs
@@ -11,6 +11,8 @@ pub use ser::to_string;
 
 #[cfg(test)]
 mod tests {
+    use crate::Value;
+
     use super::*;
     use serde::{Deserialize, Serialize};
     #[derive(Serialize, Deserialize, PartialEq, Debug)]
@@ -33,5 +35,15 @@ mod tests {
 
         let deserialized: Person = from_str(&json_str).unwrap();
         assert_eq!(deserialized, person);
+    }
+
+    #[test]
+    fn test_serde_value() {
+        let json_str = r#"{"name":"Alice","age":30,"hobbies":["reading","coding"]}"#;
+        let v = Value::parse(json_str).unwrap();
+
+        let person1: Person = from_str(&to_string(&v).unwrap()).unwrap();
+        let person2: Person = from_str(&json_str).unwrap();
+        assert_eq!(person1, person2);
     }
 }
