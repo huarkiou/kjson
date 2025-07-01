a612f0f22bd0f9aaad5af3d1695f97bd4d2d6752
huarkiou <huarkiou@outlook.com>
2025-07-01 15:38:59 +0800

    使serde成为可选feature

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,13 @@ mod context;
 mod dict;
 mod error;
 mod number;
-mod serde_support;
 mod stack;
 mod value;
 
 pub use crate::error::ParseError;
 pub use crate::value::Value;
+
+#[cfg(feature = "serde")]
+mod serde_support;
+#[cfg(feature = "serde")]
+pub use serde_support::*;
